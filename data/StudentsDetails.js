8e425064fc5b9a7731c871e01229f48ebbc878e6
tushargola1 <87809407+tushargola1@users.noreply.github.com>
2026-08-12 16:05:40 +0530

    new studetns data update

---
+++ data/StudentsDetails.js
@@ -0,0 +1,203 @@
+export const studentsDetails = [
+  {
+    id: "1",
+    name: "Gopal Patodia",
+    designation: "Sr Manager NTPC Ltd",
+    image: "/assets/images/students/1.jpg",
+    about:
+      "I am Gopal Patodia working as Senior Manager at NTPC Ltd, with over 15 years of experience in operation and maintenance of thermal power stations.",
+  },
+  {
+    id: "2",
+    name: "Vaibhav",
+    designation: "DGM (Energy and Efficiency Management Group), NTPC Kudgi",
+    image: "/assets/images/students/2.jpg",
+    about:
+      "I have been working in EEMG for past 8 years covering all aspects of the department. Performance guarantee testing of all coal thermal power plant systems have been my most rewarding experiences.",
+  },
+  {
+    id: "3",
+    name: "Nalin Garg",
+    designation: "DGM, NTPC Limited",
+    image: "/assets/images/students/3.jpeg",
+    about:
+      "I am presently serving NTPC as DGM in Operations department. I have vast Project Engineering and Operational experience in sub-critical and super-critical thermal power plants. I also have a keen interest in Digital applications and have developed useful tools in Visual Studio, mobile Android applications and learning projects in AI/ML using Python. I wish to learn more about AI technology and how it can benefit India and NTPC in transitioning from a largely thermal power-based economy to a more diversified non-fossil clean energy ecosystem.",
+  },
+  {
+    id: "4",
+    name: "Niranjan Reddy Arachatla",
+    designation: "Deputy General Manager, NTPC Ltd",
+    image: "/assets/images/students/4.jpeg",
+    about:
+      "I am a Deputy General Manager at NTPC Limited, currently posted at NTPC Solapur. I have completed my B.E. in Electrical and Electronics Engineering and an M.Tech. in Power Electronics and Drives. Over the past 15 years, I have gained diverse experience in Main Plant Operations, Ash Handling Plant Maintenance, and Coal Handling Plant Maintenance across NTPC Simhadri, Farakka, and Solapur.",
+  },
+  {
+    id: "5",
+    name: "R.V. Kiran",
+    designation: "DGM (Ash Handling), PGDM(E) Pursuing",
+    image: "/assets/images/students/5.jpg",
+    about:
+      "I am DGM (Ash Handling Department) with experience in Operation & Ash Handling Maintenance of Thermal Power Plant. My interests include Green Hydrogen, applying quantitative methods, and programming.",
+  },
+  {
+    id: "6",
+    name: "Guru Prasad Deheri",
+    designation:
+      "Senior Manager, Civil Construction Department, Lara Super Thermal Power Projects, NTPC Ltd",
+    image: "/assets/images/students/6.jpg",
+    about:
+      "I am currently working as Senior Manager in Civil Construction Department, Projects Group of NTPC Lara looking after Stage-II Township erection works and contract closing activity since May-2024. Prior to this, I worked at NTPC Darlipali for 6 years in Township construction and CSR works and 6 years in NTPC Kaniha engaged in coal coordination cell of Fuel Management group. I have expertise in civil contract execution, quality management, bill processing, correspondence, time extension, quantity deviation, contract closing procedures, and SAP systems.",
+  },
+  {
+    id: "7",
+    name: "Roshan Kumar Singh",
+    designation: "Senior Manager, NTPC Ltd. | Executive Student, NSB",
+    image: "/assets/images/students/7.jpg",
+    about:
+      "Senior Manager at NTPC Ltd. Vindhyachal, Power Manager with experience in commissioning, O&M, and commercial functions at Supercritical and sub-critical thermal units. Certified Energy Auditor and currently pursuing Executive PG Diploma from NSB.",
+  },
+  {
+    id: "8",
+    name: "Arun Tiwary",
+    designation: "Deputy General Manager – Projects, NTECL Vallur (NTPC)",
+    image: "/assets/images/students/8.jpg",
+    about:
+      "I am a Deputy General Manager – Projects at NTECL Vallur (NTPC) with 17+ years of experience in erection and commissioning of C&I systems across thermal power stations. I am PMP-certified with expertise in DCS platforms, plant commissioning, and energy management. Presently pursuing PGDM (Energy Management) at NSB.",
+  },
+  {
+    id: "9",
+    name: "Robin Singh Katariya",
+    designation: "Manager (Operation), NTPC Limited",
+    image: "/assets/images/students/9.jpeg",
+    about:
+      "I am Manager at NTPC Limited with expertise in commissioning and operation of supercritical thermal power plants. My interests include power sector transformation, sustainable energy, operational excellence, and strategic management.",
+  },
+  {
+    id: "10",
+    name: "Anuj Sankaran K",
+    designation: "Sr Manager, NTPC",
+    image: "/assets/images/students/10.jpeg",
+    about:
+      "I am a Senior Manager at NTPC with over 15 years of experience in combined cycle and solar power plant operations. My professional interests include energy system modelling, renewable energy integration, power system optimization, and application of AI and data analytics to improve operational efficiency.",
+  },
+  {
+    id: "11",
+    name: "Shivam Mishra",
+    designation: "Sr Manager, O&M-MM, NTPC Farakka",
+    image: "/assets/images/students/11.jpg",
+    about:
+      "I am a Senior Manager at NTPC-Farakka, working in the field of mechanical maintenance. In my current work profile, I am looking after mill maintenance activities.",
+  },
+  {
+    id: "12",
+    name: "A Anoop Kumar",
+    designation: "DGM (Operation)",
+    image: "/assets/images/students/12.jpg",
+    about:
+      "I am DGM Operation with experience in 500MW units of BHEL and Ansaldo once-through boilers along with certifications of BOE and Energy Auditor. My present focus after joining NSB for PGDM(E) is to learn about prospects of the electricity market undergoing energy transition and apply it for future diversified roles at NTPC.",
+  },
+  {
+    id: "13",
+    name: "Mayank Kumar Singh",
+    designation: "Deputy General Manager, NTPC Ltd.",
+    image: "/assets/images/students/13.jpg",
+    about:
+      "Mayank Kumar Singh is a Deputy General Manager (Control & Instrumentation) at NTPC with 17 years of experience in erection, commissioning, operation, and maintenance of large thermal power plants. He has led critical initiatives in plant reliability, automation, and process optimization.",
+  },
+  {
+    id: "14",
+    name: "Yoginderkumar J",
+    designation: "Senior Manager, NTPC Limited",
+    image: "/assets/images/students/14.jpg",
+    about:
+      "Senior Manager at NTPC Limited with 15 years of experience, including 12 years in thermal power plant operations, 2 years in infrastructure development, and one year of nuclear power training at NPCIL. Currently pursuing PGDM(E) at NSB to strengthen strategic management and leadership capabilities.",
+  },
+  {
+    id: "15",
+    name: "Sumit Kumar Gaurav",
+    designation: "Manager(M), Damodar Valley Corporation",
+    image: "/assets/images/students/5.jpg",
+    about:
+      "Power sector professional with 13 years experience in Construction, Commissioning and Operation of 500 MW Thermal Power plant. A keen learner exploring the management aspect of the Energy sector through the NSB PGDM(E) in Energy Management.",
+  },
+  {
+    id: "16",
+    name: "Mithilesh Kumar",
+    designation: "Chief Manager (Mining), Northern CoalFields Limited",
+    image: "/assets/images/students/16.png",
+    about:
+      "I am a Chief Manager mining at NCL, a subsidiary of Coal India Limited, with 19 years experience in coal production operations. I have worked in various mining operations with a strong focus on safety, production, environmental management, and operational efficiency.",
+  },
+  {
+    id: "17",
+    name: "Abhinav Bakhla",
+    designation: "Sr. Manager, O&M TMD, NTPC Farakka",
+    image: "/assets/images/students/17.png",
+    about:
+      "I am a senior manager at NTPC Ltd with 15 years of expertise in main plant operation and turbine maintenance.",
+  },
+  {
+    id: "18",
+    name: "AMIT CHAUHAN",
+    designation: "Manager, Fuel Management, DVC",
+    image: "/assets/images/students/18.jpeg",
+    about:
+      "I am a Manager at DVC, MTPS with experience in Fuel Management and Plant Operations. My work involves data processing, analysis and energy charge formulation.",
+  },
+  {
+    id: "19",
+    name: "S M Sundaram",
+    designation: "Chief Manager (Excavation), CMPDI",
+    image: "/assets/images/students/19.jpg",
+    about:
+      "My expertise is in preparation of Detailed Project Report & Mining Plans of Opencast Coal Mine projects.",
+  },
+  {
+    id: "20",
+    name: "Abhaya Kumar Jha",
+    designation: "Dy. General Manager (Technical), NEEPCO Ltd.",
+    image: "/assets/images/students/20.jpeg",
+    about:
+      "I am Dy. General Manager (Technical) in NEEPCO Ltd. with expertise in construction of Hydro Power Projects from work's commencement to Arbitration & finally, Contract closure.",
+  },
+  {
+    id: "21",
+    name: "Pawan Kothiwal",
+    designation: "Senior Manager - RTI, CC-EOC/NOIDA, NTPC",
+    image: "/assets/images/students/21.jpg",
+    about:
+      "Pawan Kothiwal, Engineer by Profession, Explorer by nature.",
+  },
+  {
+    id: "22",
+    name: "APPASO PARIT",
+    designation: "Sr. Manager, O&M, NTPC Ltd.",
+    image: "/assets/images/students/22.jpg",
+    about:
+      "I am a power sector professional with experience in power plant operations, particularly in gas-based and thermal power generation.",
+  },
+  {
+    id: "23",
+    name: "VISHWAS SINGH",
+    designation: "Senior Manager, NTPC Limited",
+    image: "/assets/images/students/23.jpeg",
+    about:
+      "Currently working as Senior Manager NTPC Limited. I have expertise in SCADA, DCS and PLC Systems, Maintenance and commissioning of Control & instrumentation system in Thermal Power Plant, integrating new technologies, inventory management, problem solving, strategic planning, budgeting, managing large teams, completing projects in time.",
+  },
+  {
+    id: "24",
+    name: "ANAND PRAKASH",
+    designation: "DGM - Consultancy, NTPC Ltd.",
+    image: "/assets/images/students/24.jpg",
+    about:
+      "I am having 17 years of experience in O&M areas of NTPC Thermal Power & One year experience in Consultancy (RDSS Scheme).",
+  },
+  {
+    id: "25",
+    name: "PREM PRAKASH",
+    designation: "DGM, Mech. Maintenance, NTPC Kanti",
+    image: "/assets/images/students/25.jpeg",
+    about:
+      "Prior to joining NSB, I have been working as DGM at NTPC Kanti, having a total experience of 17 years, across various NTPC projects.",
+  },
+];
